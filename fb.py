04b4bb94c23d1a152c41d0708f270b99df4596e9
Balazs Koczina <35107451+balazskoczina@users.noreply.github.com>
2018-06-13 11:53:40 +0200

    cleansed code

--- fb.py
+++ fb.py
@@ -1,23 +1,18 @@
 from facebook_business.adobjects.adaccount import AdAccount
 from facebook_business.adobjects.adsinsights import AdsInsights
+from facebook_business.api import  FacebookAdsApi
 from facebook_business.adobjects.adreportrun import AdReportRun
 import time
-import facebook_business
 import datetime
 import json
 
-api = facebook_business.api.FacebookAdsApi.init(access_token='xxx')
-account_id = 'act_xxx'
-
-account = AdAccount(account_id)
-
+api = FacebookAdsApi.init(access_token='xxxxx')
+account = AdAccount('xxxxx')
 today = datetime.date.today()
 startDate=str(today - datetime.timedelta(days=3))
 endDate=str(today - datetime.timedelta(days=3))
-
 fileName="facebook_api_"+startDate+".json"
-
-row = 1
+data = []
 
 params = {
     'level': AdsInsights.Level.ad,
@@ -26,29 +21,27 @@ params = {
         'until': endDate,
     },
     'fields': [
-        AdsInsights.Field.ad_name,
         AdsInsights.Field.ad_id,
+        AdsInsights.Field.ad_name,
         AdsInsights.Field.adset_name,
-        AdsInsights.Field.campaign_name,
         AdsInsights.Field.campaign_id,
-        AdsInsights.Field.spend,
+        AdsInsights.Field.campaign_name,
         AdsInsights.Field.reach,
+        AdsInsights.Field.spend,
         AdsInsights.Field.unique_clicks,
+        AdsInsights.Field.clicks,
         AdsInsights.Field.impressions,
         AdsInsights.Field.cpc,
         AdsInsights.Field.cpm,
         AdsInsights.Field.actions
     ],
     'breakdowns':['age','gender'],
-    # 'export_format': 'json',
-    'limit': 4000
+    'export_format': 'json',
+    'limit': 1000
 }
 
 async_job = account.get_insights(params=params,async=True)
-
 async_job.remote_read()
-
-data = []
 
 while async_job[AdReportRun.Field.async_percent_completion] < 100:
     async_job.remote_read()
@@ -56,15 +49,8 @@ while async_job[AdReportRun.Field.async_percent_completion] < 100:
 
 result = async_job.get_result()
 
+for AdsInsights in result:
+    data.append(AdsInsights.export_all_data())
+
 with open(fileName, 'w') as outfile:
-    for AdsInsights in result:
-        json.dump(AdsInsights.export_all_data(), outfile)
-
-# json_output = json.dumps(async_job.get_result())
-
-# print(async_job.get_result())
-# print(async_job.export_all_data())
-# print(type(async_job.get_result()))
-# print(data)
-# with open(fileName, 'w') as outfile:
-#     json.dump(async_job.get_result(), outfile)
+    json.dump(data, outfile, sort_keys = True, ensure_ascii=True, indent=0, separators=(',',':'))
